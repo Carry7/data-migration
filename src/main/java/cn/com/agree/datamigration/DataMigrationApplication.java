0cba2523a72942289da73aabb1725459d7a3463f
Carry7 <1315175354@qq.com>
2020-04-10 17:49:30 +0800

    init

---
+++ src/main/java/cn/com/agree/datamigration/DataMigrationApplication.java
@@ -0,0 +1,13 @@
+package cn.com.agree.datamigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMigrationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cn/com/agree/datamigration/mapper/AccessservicerulesMapper.java
@@ -0,0 +1,15 @@
+package cn.com.agree.datamigration.mapper;
+
+import cn.com.agree.datamigration.entity.Accessservicerules;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface AccessservicerulesMapper {
+    @Select("select * from aecip_ipc_accessservicerules ")
+    public List<Accessservicerules> getTest();
+}
